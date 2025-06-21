7ee342ecc0ee19ab8175c2b6479fef400a349ee1
MJLA <marlonjeshitlopesarmero@gmail.com>
2025-06-20 21:29:45 -0500

    20-06-2025

--- dices.py
+++ dices.py
@@ -10,13 +10,17 @@
 import os 
 from random import randint, uniform
 
-os.system('clear')
-print("::: WELCOM TO MY PARCHIS :::")
+# Functions #################################
+def get_dices():
+    os.system('clear')
+    print("::: WELCOM TO MY PARCHIS :::")
 
-# print("Player name:")
-player_name = input("Player name:")
-dice1 = randint(1,6)
-dice2 = randint(1,6)
+    # print("Player name:")
+    player_name = input("Player name:")
+    dice1 = randint(1,6)
+    dice2 = randint(1,6)
 
-print(f"Dice 1: {dice1}")
-print(f"Dice 2: {dice2}")
+    print(f"Dice 1: {dice1}")       # print("Dice 1: ", dice1)
+    print(f"Dice 2: {dice2}")       # print("Dice 2: ", dice2)
+
+get_dices()
